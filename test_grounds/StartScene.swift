c8d82bb60d7c3bf160f6fe25d24e4682339d80ff
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2021-01-13 11:14:06 -0400

    keeping the branch for reference an ideas

--- test_grounds/StartScene.swift
+++ test_grounds/StartScene.swift
@@ -14,9 +14,9 @@ import UIKit
 
 class StartScene: SKScene{
     
-    var puertoRicobp: UIBezierPath! = TestClass().createMunicipiosBezierPath()//TestClass().createYauco()    var municipiobp
+
     
-    //TestClass().createPRMap()
+
     
 
     
@@ -26,21 +26,20 @@ class StartScene: SKScene{
     
      override func didMove(to view: SKView){
         
-        self.backgroundColor = SKColor.blue
-        let puertoRicoSKSpriteNode: SKSpriteNode = TestClass().drawMunicipiosBezierPath(bpPuertoRico: puertoRicobp)
-        puertoRicoSKSpriteNode.xScale = -1.0
-       puertoRicoSKSpriteNode.zRotation = 9.5
-      puertoRicoSKSpriteNode.position = CGPoint(x:100, y: 100)
-        self.addChild( puertoRicoSKSpriteNode)
-       // let shapeNode = SKShapeNode(path: municipiobp.cgPath)
-        //shapeNode.fillColor = UIColor.red
-        //shapeNode.strokeColor = UIColor.white
-        //shapeNode.position = CGPoint(x: 0, y: 0)
-       // let view = SKView(frame: UIScreen.main.bounds)
-        //let texture = view.texture(from: shapeNode)!
-       // let municipioNode = SKSpriteNode(texture: texture)
-       // self.addChild(municipioNode)    }
+        TestClass().createCaboRojoBezierPath()
+        TestClass().caboRojoBPToSKSpriteNode(bpCaboRojo: TestClass().caboRojoBP)
+        self.addChild(TestClass().caboRojoSKSpriteNode)
         
+
     }
 
 }
+
+    //var puertoRicobp: UIBezierPath! = TestClass().createMunicipiosBezierPath()//TestClass().createYauco()    var municipiobp
+  /*self.backgroundColor = SKColor.blue
+  let puertoRicoSKSpriteNode: SKSpriteNode = TestClass().drawMunicipiosBezierPath(bpPuertoRico: puertoRicobp)
+  puertoRicoSKSpriteNode.xScale = -1.0
+ puertoRicoSKSpriteNode.zRotation = 9.5
+puertoRicoSKSpriteNode.position = CGPoint(x:100, y: 100)
+  self.addChild( puertoRicoSKSpriteNode)*/
+
