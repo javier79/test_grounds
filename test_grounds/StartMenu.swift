6a14f346341e084aa69629032b23f294f53a58bc
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2022-03-19 10:33:14 -0400

    este commit es solo para anadir la creacion de la escena StartMenuScene que se habia quedado colgando en el branch anterior ya que olvide primero crear este branch y hacerlo en este branch a donde corresponde, nada muy importante

---
+++ test_grounds/StartMenu.swift
@@ -0,0 +1,13 @@
+//
+//  StartMenu.swift
+//  test_grounds
+//
+//  Created by javier pizarro on 3/19/22.
+//  Copyright © 2022 javierpizarro. All rights reserved.
+//
+
+import SpriteKit
+
+class StartMenuScene: SKScene {
+    
+}
